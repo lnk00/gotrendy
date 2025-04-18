1095bb3951faad13d9a7cc48f06a831c8a2d870b
Damien Dumontet <damiendumontet@192.168.68.105>
2025-04-18 18:44:17 -0300

    remove text animation

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,5 @@
-import { BlurFade } from "@/components/ui/blur-fade";
 import { DotPattern } from "@/components/ui/dot-pattern";
 import { InteractiveHoverButton } from "@/components/ui/interactive-hover-button";
-import { TextAnimate } from "@/components/ui/text-animate";
 import { cn } from "@/lib/utils";
 import { createFileRoute } from "@tanstack/react-router";
 
@@ -21,40 +19,20 @@ function RouteComponent() {
 						"[mask-image:radial-gradient(1500px_circle_at_center,transparent,white)]",
 					)}
 				/>
-				<TextAnimate
-					as={"h1"}
-					className="font-bold text-6xl"
-					animation="blurInDown"
-					by="character"
-					duration={0.5}
-				>
+				<h1 className="font-bold text-6xl">
 					The content engine for modern creators
-				</TextAnimate>
-				<TextAnimate
-					as={"h2"}
-					className="text-2xl max-w-1/2 text-center mt-8 font-medium text-muted-foreground"
-					animation="blurInUp"
-					by="character"
-					duration={0.5}
-				>
+				</h1>
+				<h2 className="text-2xl max-w-1/2 text-center mt-8 font-medium text-muted-foreground">
 					Get personalized content ideas, powered by your
-				</TextAnimate>
-				<TextAnimate
-					as={"h2"}
-					className="text-2xl max-w-1/2 text-center font-medium text-muted-foreground"
-					animation="blurInUp"
-					by="character"
-					duration={0.5}
-				>
+				</h2>
+				<h2 className="text-2xl max-w-1/2 text-center font-medium text-muted-foreground">
 					browsing and social feeds.
-				</TextAnimate>
-				<BlurFade delay={0.5} duration={0.5}>
-					<InteractiveHoverButton className="mt-8">
-						Get started now
-					</InteractiveHoverButton>
-				</BlurFade>
+				</h2>
+				<InteractiveHoverButton className="mt-8">
+					Get started now
+				</InteractiveHoverButton>
 			</div>
-			<div className="border-b relative flex h-[180px] w-full flex-col items-center justify-center overflow-hidden bg-blue-600">
+			<div className="border-b relative flex h-[180px] w-full flex-col items-center justify-center overflow-hidden bg-primary">
 				<span className="font-bold text-4xl max-w-1/2 text-center text-white">
 					Stay inspired with topics that truly resonate with your audience.
 				</span>
