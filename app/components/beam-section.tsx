a6e499c3937388f8ab5cd9459860ab307ad12469
Damien Dumontet <damiendumontet@192.168.68.105>
2025-04-19 15:22:03 -0300

    add shadow to linkedin post

--- app/components/beam-section.tsx
+++ app/components/beam-section.tsx
@@ -25,7 +25,7 @@ function SocialFeed({ ref, img, className }: IPropsSocialFeed) {
 
 function LinkedinPost() {
 	return (
-		<div className="w-full border rounded-lg p-4 relative">
+		<div className="w-full border rounded-lg p-4 relative [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]">
 			<div className="flex gap-2 items-start">
 				<Avatar>
 					<AvatarImage src="assets/images/avatar.png" alt="avatar" />
